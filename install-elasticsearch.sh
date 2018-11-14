43f252b49e7a11b9f837022ae37f2631cc7cd66f
TeeDeeWee <45033393+TeeDeeWee@users.noreply.github.com>
2018-11-14 18:33:43 +0100

    Update install-elasticsearch.sh

--- install-elasticsearch.sh
+++ install-elasticsearch.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "Installatie van Elasticsearch."
 echo "Installeren van de public signing key."
-wget -qO https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 echo "Installeren van benodigde packages."
+sudo apt-get install default-jre
 sudo apt-get install apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
 echo "Installeren van Elasticsearch package."
