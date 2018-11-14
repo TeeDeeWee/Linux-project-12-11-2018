b7ac71d553ed89022f1c9bffd5c03ee44c8b24eb
TeeDeeWee <45033393+TeeDeeWee@users.noreply.github.com>
2018-11-14 19:03:07 +0100

    Update install-kibana.sh

---
+++ install-kibana.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Installatie van Kibana."
+echo "deb http://packages.elastic.co/kibana/4.5/debian stable main" | sudo tee -a /etc/apt/sources.list.d/kibana-4.5.x.list
+echo "Updaten van de package database."
+sudo apt-get update
+echo "Installeren van Kibana package."
+sudo apt-get -y install kibana
+echo "Aanpassen van config files."
+echo "server.host: 127.0.0.1" >> /opt/kibana/config/kibana.yml
+echo "Starten van de kibana service."
+sudo update-rc.d kibana defaults 96 9
+sudo systemctl start kibana
+echo "Controleren van Kibana status."
+sudo systemctl status kibana
