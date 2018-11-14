790683e1b1e3273aa80e5e1bf8fd56736bb6e507
TeeDeeWee <45033393+TeeDeeWee@users.noreply.github.com>
2018-11-14 11:53:38 +0100

    Docker script
    Typfoutjes

--- install-docker-master.sh
+++ install-docker-master.sh
@@ -4,9 +4,9 @@ echo "Toevoegen van de repository."
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $lsb_release -cs) stable"
 echo "Updaten van de package index."
-sudo apt-get Update
+sudo apt-get update
 echo "De juiste repository kiezen."
-apt-cahe policy docker-ce
+apt-cache policy docker-ce
 echo "Installeren van Docker."
 sudo apt-get install -y docker-ce
 echo "De status van Docker controleren."
