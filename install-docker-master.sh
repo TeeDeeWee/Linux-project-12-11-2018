b89713b5e2f8ab3a5b317d2b4f67b852ae6c2cb5
TeeDeeWee <45033393+TeeDeeWee@users.noreply.github.com>
2018-11-15 16:24:07 +0100

    Versie aangepast

--- install-docker-master.sh
+++ install-docker-master.sh
@@ -4,12 +4,12 @@ echo "Benodigde packages installeren."
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
 echo "Toevoegen van de repository."
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable-17.06"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
 echo "Updaten van de package index."
 sudo apt-get update
 echo "De juiste repository kiezen."
 apt-cache policy docker-ce
 echo "Installeren van Docker."
-yes | sudo apt-get install -y docker-ce
+yes | sudo apt-get install docker-ce=17.06.0~ce-0~ubuntu
 echo "De status van Docker controleren."
 sudo systemctl status docker
