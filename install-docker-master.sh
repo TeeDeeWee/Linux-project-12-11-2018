4b6cf8ab3d853b2f781e469d0e36ff2d5e6afc30
TeeDeeWee <45033393+TeeDeeWee@users.noreply.github.com>
2018-11-15 16:37:18 +0100

    Swap uitzetten

--- install-docker-master.sh
+++ install-docker-master.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Installatie van Docker."
 echo "Benodigde packages installeren."
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+yes | sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
 echo "Toevoegen van de repository."
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
--- install-kubernetes.sh
+++ install-kubernetes.sh
@@ -7,5 +7,7 @@ echo "Updaten van de repository."
 sudo apt-get update -y
 echo "Installeren van Kubernetes."
 yes | sudo apt-get install kubelet kubeadm kubectl -y
+echo "Swap uitzetten."
+sudo swapoff -a
 echo "Installatie van kubernetes testen."
 sudo kubeadm init --pod-network-cidr=192.168.56.0/24 --apiserver-advertise-address=192.168.0.101
